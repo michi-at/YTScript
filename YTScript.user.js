0fab22946005e99f5b822d5f55011a1b650b1baf
michi-at <lookforsoul1@gmail.com>
2019-01-11 00:03:31 +0200

    initial commit

---
+++ YTScript.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         YTScript
+// @description  YouTube player enhancement
+// @author       michi-at
+// @version      0.0.0
+// @updateURL    ""
+// @downloadURL  ""
+// @match        *://www.youtube.com/watch*
+// @exclude      *://www.youtube.com/embed/*
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @run-at       document-start
+// ==/UserScript==
+
+(function () {
+    "use strict";
+
+    
+})();
